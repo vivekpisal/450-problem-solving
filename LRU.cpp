83fb19f0d6639eeb5cf9520e00a05bb1d173da73
vivekpisal <54746512+vivekpisal@users.noreply.github.com>
2020-12-05 10:02:00 +0530

    Add files via upload

--- LRU.cpp
+++ LRU.cpp
@@ -2,8 +2,28 @@
 #include<algorithm>
 #include<bits/stdc++.h>
 using namespace std;
+class node
+{
+public:
+	int data;
+	node *next=nullptr;
+	node *back=nullptr;
+	node(int data):data{data}{}
+	node(){}
+};
+void show(node *temp)
+{
+	while(temp!=NULL)
+	{
+		cout<<temp->data<<" ";
+		temp=temp->next;
+	}
+	cout<<endl;
+}
 int main()
 {
+	//FIRST APPROACH
+	/*
 	int size=3;
 	int a[size];
 	int page,paging_fault=0,cache_head=0;
@@ -41,6 +61,70 @@ int main()
 				paging_fault++;
 			}
 		}
+	}*/
+	/* SECOND APRAOCH
+	Optimized the right shift using doubly linked list.
+	*/
+	node *front=NULL,*rear=front;
+	int page,size=3,count_size=0;
+	while(1)
+	{
+		cin>>page;
+		if(page==10)
+			break;
+		if(count_size<3)
+		{
+			node *newnode=new node(page);
+			if(front==NULL)
+			{
+				front=newnode;
+				newnode->back=front;
+				newnode->next=NULL;
+				count_size++;
+			}
+			else
+			{
+				node *temp=front;
+				newnode->next=temp;
+				temp->back=newnode;
+				front=newnode;
+				newnode->back=front;
+				count_size++;
+			}
+			if(count_size==2)
+				rear=front->next;
+		}
+		else
+		{
+			node *temp=front;
+			bool flag=false;
+			while(temp!=NULL)
+			{
+				if(temp->data==page)
+				{
+					flag=true;
+					cout<<"found\n";
+					break;
+				}
+				else
+					flag=false;
+				temp=temp->next;
+			}
+			if(flag==false)
+			{
+				cout<<"not";
+				node *temp=front;
+				node *newnode=new node(page);
+				front=newnode;
+				newnode->back=front;
+				newnode->next=temp;
+				temp=rear->back;
+				delete rear;
+				rear=temp;
+			}
+		}
+		show(front);
 	}
+	show(front);
 	return 0;
 }
